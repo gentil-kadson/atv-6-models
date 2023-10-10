ac7209bb4825313c81da7433551d02fdac7afa35
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-10-10 10:18:50 -0300

    overloaded str methods and added some entries to all models.

--- apps/sebo_de_discos/admin.py
+++ apps/sebo_de_discos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Disco
+from .models import Disco, Artista, SeloFonografico
 
 # Register your models here.
 admin.site.register(Disco)
+admin.site.register(Artista)
+admin.site.register(SeloFonografico)
--- apps/sebo_de_discos/models.py
+++ apps/sebo_de_discos/models.py
@@ -6,19 +6,28 @@ from django.db import models
 class SeloFonografico(models.Model):
     titulo = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.titulo
+
 
 class Disco(models.Model):
     nome = models.TextField(max_length=200)
     descricao = models.TextField(max_length=300)
     selo_fonografico = models.ForeignKey(
-        SeloFonografico, on_delete=models.CASCADE)
+        SeloFonografico, on_delete=models.CASCADE, related_name="discos")
     ano = models.IntegerField()
     pais = models.TextField(max_length=80)
     genero = models.TextField(max_length=70)
     quantidade = models.BigIntegerField()
+
+    def __str__(self):
+        return self.nome
 
 
 class Artista(models.Model):
     nome = models.CharField(max_length=30)
     idade = models.PositiveIntegerField()
     discos = models.ManyToManyField(Disco)
+
+    def __str__(self):
+        return self.nome
