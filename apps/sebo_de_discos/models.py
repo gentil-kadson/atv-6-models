a4d88418bab7e6059b47c4cb322ec71f07502b4b
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-10-10 09:19:12 -0300

    created 'Artista' and 'SeloFonografico' models.

--- apps/sebo_de_discos/models.py
+++ apps/sebo_de_discos/models.py
@@ -3,11 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+class SeloFonografico(models.Model):
+    titulo = models.CharField(max_length=30)
+
+
 class Disco(models.Model):
     nome = models.TextField(max_length=200)
     descricao = models.TextField(max_length=300)
-    selo_fonografico = models.TextField(max_length=100)
+    selo_fonografico = models.ForeignKey(
+        SeloFonografico, on_delete=models.CASCADE)
     ano = models.IntegerField()
     pais = models.TextField(max_length=80)
     genero = models.TextField(max_length=70)
     quantidade = models.BigIntegerField()
+
+
+class Artista(models.Model):
+    nome = models.CharField(max_length=30)
+    idade = models.PositiveIntegerField()
+    discos = models.ManyToManyField(Disco)
