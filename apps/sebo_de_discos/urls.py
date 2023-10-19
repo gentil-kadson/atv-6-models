2bc0849507d818877f4d75bc5eba46e04aa2b508
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-10-19 16:20:59 -0300

    terminei de montar boa parte da pagina dos detalhes do discos.

--- apps/sebo_de_discos/urls.py
+++ apps/sebo_de_discos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from apps.sebo_de_discos import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('disco/<int:id_disco>/', views.disco, name='disco')
 ]
--- apps/sebo_de_discos/views.py
+++ apps/sebo_de_discos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Disco
+from .models import Disco, Artista, SeloFonografico
 
 # Create your views here.
 def index(request):
@@ -7,4 +7,12 @@ def index(request):
     context = { 'discos': discos }
 
     return render(request, 'lista_discos.html', context)
+
+def disco(request, id_disco):
+    disco = Disco.objects.get(id=id_disco)
+    artistas = Artista.objects.filter(discos__id__in=[disco.id])
+
+    context = { 'disco': disco, 'artistas': artistas }
+
+    return render(request, 'detalhes_disco.html', context)
 
